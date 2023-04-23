1c142bbe29f29e80b666dea40b6ef1f1be96543f
AbylaiNur <abylai.nurskenev@gmail.com>
2023-04-23 23:39:38 +0600

    changes:
     - removed bot state and bot last active command from migraions

--- scrapper/migrations/chat.sql
+++ scrapper/migrations/chat.sql
@@ -1,9 +1,5 @@
 CREATE TABLE chat (
     id                      BIGINT          PRIMARY KEY,
-    bot_state               VARCHAR(255),
-    bot_last_active_command VARCHAR(255)
 );
 
 COMMENT ON COLUMN chat.id IS 'id телеграм чата';
-COMMENT ON COLUMN chat.bot_state IS 'Определенное состояние бота после последней команды пользователя';
-COMMENT ON COLUMN chat.bot_last_active_command IS 'Последняя команда пользователя';
